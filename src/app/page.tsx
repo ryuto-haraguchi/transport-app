d177ab05b15f83258c12efc83f6b14159c5346cf
ryuto <hr48691226@gmail.com>
2025-05-11 14:31:12 +0900

    shadcn/uiを導入, node:20-bullseye-slimを使用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      
+      {/* 再利用できるように修正する */}
+      <Button variant="destructive">Click me</Button>
     </div>
   );
 }
